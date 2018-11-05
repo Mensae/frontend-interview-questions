a27bd9f7cfab7bff78fb9b1e9e99835647ecd104
Johan Meester <walter.doodah@gmail.com>
2018-11-05 10:08:17 +0100

    Changing jsDoc description

--- coding-easy/reverse.js
+++ coding-easy/reverse.js
@@ -2,11 +2,6 @@
  * Reverses the given string (yes, using the built in reverse function is
  * cheating).
  *
- * Note: This solution aims for clarity, but does not give correct results when
- * given unicode characters from the supplementary planes (including Chinese
- * characters, emojis, etc.). See discussion here:
- * @see {@link https://github.com/bcherny/frontend-interview-questions/issues/6}
- *
  * @param string
  * @returns {string}
  */
